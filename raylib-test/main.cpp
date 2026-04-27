27407bf577234b74685e530562b449b491dad8cb
Luciano <luciano_flores42@hotmail.com>
2026-04-27 10:42:07 -0300

    archivos movidos a carpeta

--- raylib-test/main.cpp
+++ raylib-test/main.cpp
@@ -48,16 +48,16 @@ int main () {
     string Materia = "Introduccion a la programación";
     cout << "y esto es " << Materia <<endl;
     */
-   /*
-   // Establezco una variable de nombre "Nombre"
-   string Nombre;
-   // Mando msj diciendo que ingrese nombre (solo legible)
-   cout << "Por favor, ingrese su nombre ";
-   // Pido el texto (aca seria la parte donde uno escribe)
-   cin >> Nombre;
-   // Mando texto + lo ingresado
-   cout << "El nombre ingresado es: " << Nombre <<endl;
-   */
+    /*
+    // Establezco una variable de nombre "Nombre"
+    string Nombre;
+    // Mando msj diciendo que ingrese nombre (solo legible)
+    cout << "Por favor, ingrese su nombre ";
+    // Pido el texto (aca seria la parte donde uno escribe)
+    cin >> Nombre;
+    // Mando texto + lo ingresado
+    cout << "El nombre ingresado es: " << Nombre <<endl;
+    */
     /*
     string a = "hola naranjas naranjosas";
     a = a.insert(5, "manzanas y ");
@@ -165,8 +165,36 @@ int main () {
     
     cout << "La operacion suma de x, el doble de y, todo mas el triple de z, dividido 6, da como resultado: " << operacion <<endl;
     */
-    // PRUEBA PARA VER SI ANDA TODO OKKKKKKKKKKKKKK
+    /*Ejercicio autoevaluable.
+    string namepj;
+    int vida = 10;
+    int runa1;
+    int runa2;
+    int runa3;
     
+    cout << "Ingrese el nombre del personaje: ";
+    cin >> namepj;
+    cout << namepj << " tiene " << vida << "HP\n";
+    
+    cout << "Ingrese un numero entre 1 y 10: \n";
+    cin >> runa2;
+    cout << "Ingrese un numero entre 1 y 10: \n";
+    cin >> runa3;
+    cout << "Ingrese un numero entre 1 y 10: \n";
+    cin >> runa1;
+    
+    vida = vida - runa1 - (runa2/2);
+    cout << "Sus puntos de vida son: " << vida << "HP\n";
+    vida = vida + (runa3/2);
+    cout << "Sus puntos de vida son: " << vida << "HP\n";
+    
+    bool pjvivo = vida >= 1;
+    
+    cout << "Su personaje sigue vivo? " << pjvivo << " (1 para si 0 para no)" << endl;
+    */
+    /*entregable.
+    cout << "Bienvenidos a Introduccion a la Programacion" << endl;
+    */
 
-  return 0;
+    return 0;
 }
