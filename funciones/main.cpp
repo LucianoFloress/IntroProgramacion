2369d22446da81ce3ce66ccdf766f300ea2d6686
LucianoFloress <luciano_flores42@hotmail.com>
2026-06-03 19:12:33 -0300

    funciones

---
+++ funciones/main.cpp
@@ -0,0 +1,95 @@
+/*ejercicios*/
+/*
+#include <iostream>
+#include <string>
+using namespace std;
+
+void mostrarMsj()
+{
+    cout << "holis" << "\n";
+}
+
+void mostrarNum(int num)
+{
+    cout << num + 50;
+}
+
+int sumar(int num1, int num2)
+{
+    return num1 + num2;
+}
+
+int main()
+{
+
+    mostrarMsj();
+    mostrarNum(5500);
+    cout << "\n";
+    cout << sumar(67, 69);
+    return 0;
+}
+*/
+
+// ejercicios 2
+/*
+#include <iostream>
+#include <string>
+using namespace std;
+
+void mostrarNum(int num){
+    cout << num;
+}
+
+void mostrarDosNum(int num1, int num2){
+    cout << num1 << "\n";
+    cout << num2 << "\n";
+}
+
+int name(int name){
+    cin >> name;
+    cout << "hola " << name << "\n";
+}
+
+int main()
+{
+    mostrarNum(67);
+    cout << "\n";
+    mostrarDosNum(6, 7);
+
+    return 0;
+}
+*/
+
+// ejercicios 3
+/*
+#include <iostream>
+#include <string>
+using namespace std;
+
+void salida(string name){
+    cout << "Hola " << name;
+}
+
+int main()
+{
+    string name;
+    getline(cin, name);
+
+    salida(name);
+
+    return 0;
+}
+*/
+//ejercicios 4
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+int main()
+{
+
+
+    return 0;
+}
