b6f464e91756634080966886a57f8c25c928c56b
LucianoFloress <luciano_flores42@hotmail.com>
2026-06-19 15:48:59 -0300

    generala terminada

--- generala/main.cpp
+++ generala/main.cpp
@@ -1,17 +1,8 @@
 #include <iostream>
 #include <string>
 using namespace std;
-/*
-el juego es una sola tirada. de ahi hay que mandar los valores que salen, a cada funcion, todo al 1, o al 2, o al 3, etc. y tambien verificar si se forma algun juego, y de formarse, se guarda el valor mas alto, para luego compararlo contra la pc.
--una funcion para hacer 5 dados, de 6 caras. ✅
--una funcion para todo al 'X', es tratar de sacar la mayor cantidad de numeros iguales para sumarlos y decidir el grupo, por ejemplo: me sale 4 4 4 4 2, me conviene guardar esos 16 para "todo al 4", otro ejemplo: me sale 5 5 2 2 2, puedo decidir, si todo al 5 (10) o todo al 2 (6).
--una funcion para full, que 3 dados sean iguales y 2 dados sean iguales. (30pts)
--una funcion para poker, que 4 dados tengan el mismo numero (40pts)
--una funcion para escalera, del 1 al 5 o del 2 al 6 (50pts)
--una funcion para la generala, todos los dados iguales (60pts)
--una funcion que tire los dados.
-*/
 
+// funciones jugador
 void tirarDados(int dados[])
 {
     for (int i = 0; i < 5; i++)
@@ -119,21 +110,27 @@ bool esEscalera(int cantidadApariciones[])
     return false;
 }
 
+// funciones PC
+
+void reiniciarApariciones(int cantidadApariciones[])
+{
+    for (int i = 0; i < 6; i++)
+    {
+        cantidadApariciones[i] = 0;
+    }
+}
+
 int main()
 {
     srand(time(NULL));
     // array para guardar el puntaje de Todo Al X
     int puntos[6];
-
     int puntosFull, puntosPoker, puntosEscalera, puntosGenerala;
-    int puntajeMax = 0;
+    int puntajeMaxJugador = 0;
     string nombreJugadaMax;
 
     // 5 dados de 6 caras
     int dados[5];
-
-    // array guarda cuantas veces sale algun numero en la tirada.
-    int cantidadApariciones[6] = {0};
 
     // tiro los dados
     tirarDados(dados);
@@ -144,18 +141,21 @@ int main()
     }
     cout << "\n";
 
+    // array que guarda cuantas veces sale algun numero en la tirada.
+    int cantidadApariciones[6] = {0};
+
     // evalua todo al X y guarda los puntos correspondientes.
     for (int i = 1; i <= 6; i++)
     {
         cout << "Todo al " << i << ": " << todoAlX(dados, i) << "\n";
         puntos[i] = todoAlX(dados, i);
     }
-    
+
     for (int i = 1; i <= 6; i++)
     {
-        if (puntos[i] > puntajeMax)
+        if (puntos[i] > puntajeMaxJugador)
         {
-            puntajeMax = puntos[i];
+            puntajeMaxJugador = puntos[i];
             if (i == 1)
             {
                 nombreJugadaMax = "Todo al 1";
@@ -190,9 +190,9 @@ int main()
     {
         cout << "Full: 30" << "\n";
         puntosFull = 30;
-        if (puntosFull > puntajeMax)
+        if (puntosFull > puntajeMaxJugador)
         {
-            puntajeMax = puntosFull;
+            puntajeMaxJugador = puntosFull;
             nombreJugadaMax = "Full";
         }
     }
@@ -205,9 +205,9 @@ int main()
     {
         cout << "Poker: 40" << "\n";
         puntosPoker = 40;
-        if (puntosPoker > puntajeMax)
+        if (puntosPoker > puntajeMaxJugador)
         {
-            puntajeMax = puntosPoker;
+            puntajeMaxJugador = puntosPoker;
             nombreJugadaMax = "Poker";
         }
     }
@@ -220,9 +220,9 @@ int main()
     {
         cout << "Escalera: 50" << "\n";
         puntosEscalera = 50;
-        if (puntosEscalera > puntajeMax)
+        if (puntosEscalera > puntajeMaxJugador)
         {
-            puntajeMax = puntosEscalera;
+            puntajeMaxJugador = puntosEscalera;
             nombreJugadaMax = "Escalera";
         }
     }
@@ -235,9 +235,9 @@ int main()
     {
         cout << "Generala: 60" << "\n";
         puntosGenerala = 60;
-        if (puntosGenerala > puntajeMax)
+        if (puntosGenerala > puntajeMaxJugador)
         {
-            puntajeMax = puntosGenerala;
+            puntajeMaxJugador = puntosGenerala;
             nombreJugadaMax = "Generala";
         }
     }
@@ -246,7 +246,139 @@ int main()
         cout << "Generala: 0" << "\n";
     }
 
-    cout << "La jugada mas alta fue: " << nombreJugadaMax << " (" << puntajeMax << ")" << "\n";
+    cout << "La jugada mas alta fue: " << nombreJugadaMax << " (" << puntajeMaxJugador << ")" << "\n\n";
+
+    // TURNO PC
+
+    int puntajeMaxPc = 0;
+    // reinicio las apariciones para la PC
+    reiniciarApariciones(cantidadApariciones);
+
+    // tiro los dados
+    tirarDados(dados);
+    cout << "Tirada PC: \n";
+    for (int i = 0; i < 5; i++)
+    {
+        cout << dados[i] << " ";
+    }
+    cout << "\n";
+
+    for (int i = 1; i <= 6; i++)
+    {
+        cout << "Todo al " << i << ": " << todoAlX(dados, i) << "\n";
+        puntos[i] = todoAlX(dados, i);
+    }
+
+    for (int i = 1; i <= 6; i++)
+    {
+        if (puntos[i] > puntajeMaxPc)
+        {
+            puntajeMaxPc = puntos[i];
+            if (i == 1)
+            {
+                nombreJugadaMax = "Todo al 1";
+            }
+            if (i == 2)
+            {
+                nombreJugadaMax = "Todo al 2";
+            }
+            if (i == 3)
+            {
+                nombreJugadaMax = "Todo al 3";
+            }
+            if (i == 4)
+            {
+                nombreJugadaMax = "Todo al 4";
+            }
+            if (i == 5)
+            {
+                nombreJugadaMax = "Todo al 5";
+            }
+            if (i == 6)
+            {
+                nombreJugadaMax = "Todo al 6";
+            }
+        }
+    }
+
+    contarApariciones(dados, cantidadApariciones);
+
+    if (esFull(cantidadApariciones))
+    {
+        cout << "Full: 30" << "\n";
+        puntosFull = 30;
+        if (puntosFull > puntajeMaxPc)
+        {
+            puntajeMaxPc = puntosFull;
+            nombreJugadaMax = "Full";
+        }
+    }
+    else
+    {
+        cout << "Full: 0" << "\n";
+    }
+
+    if (esPoker(cantidadApariciones))
+    {
+        cout << "Poker: 40" << "\n";
+        puntosPoker = 40;
+        if (puntosPoker > puntajeMaxPc)
+        {
+            puntajeMaxPc = puntosPoker;
+            nombreJugadaMax = "Poker";
+        }
+    }
+    else
+    {
+        cout << "Poker: 0" << "\n";
+    }
+
+    if (esEscalera(cantidadApariciones))
+    {
+        cout << "Escalera: 50" << "\n";
+        puntosEscalera = 50;
+        if (puntosEscalera > puntajeMaxPc)
+        {
+            puntajeMaxPc = puntosEscalera;
+            nombreJugadaMax = "Escalera";
+        }
+    }
+    else
+    {
+        cout << "Escalera: 0" << "\n";
+    }
+
+    if (esGenerala(cantidadApariciones))
+    {
+        cout << "Generala: 60" << "\n";
+        puntosGenerala = 60;
+        if (puntosGenerala > puntajeMaxPc)
+        {
+            puntajeMaxPc = puntosGenerala;
+            nombreJugadaMax = "Generala";
+        }
+    }
+    else
+    {
+        cout << "Generala: 0" << "\n";
+    }
+
+    cout << "La jugada mas alta fue: " << nombreJugadaMax << " (" << puntajeMaxPc << ")" << "\n\n";
+
+    // comparamos y verificamos quien gana
+
+    if (puntajeMaxJugador > puntajeMaxPc)
+    {
+        cout << "Gana el jugador" << "\n";
+    }
+    else if (puntajeMaxJugador == puntajeMaxPc)
+    {
+        cout << "Empate" << "\n";
+    }
+    else
+    {
+        cout << "Gana la Pc" << "\n";
+    }
 
     return 0;
 }
